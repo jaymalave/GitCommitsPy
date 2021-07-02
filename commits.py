9e7e611dafd7330eaaf821ab353b97fe5ea3c0e1
jaymalave <jaym.github@gmail.com>
2021-07-02 21:09:59 +0530

    cli version done

--- commits.py
+++ commits.py
@@ -3,13 +3,35 @@ import sqlite3
 conn = sqlite3.connect('commits.db')
 cursor = conn.cursor()
 
-date = input("Enter the date: ")
-commits = int(input("Enter the no. of commits: "))
+def add():
+ date = input("Enter the date: ")
+ commits = int(input("Enter the no. of commits: "))
 
-cursor.execute('INSERT INTO commits(DATE, COMMITS) VALUES (?, ?)', (  date  ,  commits   ))
+ cursor.execute('INSERT INTO commits(DATE, COMMITS) VALUES (?, ?)', (  date  ,  commits   ))
 
-conn.commit()   
-print("success")
+ conn.commit()   
+ print("success")
 
-conn.close()
+def view():
+  for row in cursor.execute('SELECT * FROM commits'):
+      date = row[0]
+      commits = row[1]
+      print(date, ':', commits)
+
+
+def close():
+  conn.close()
+
+ask = input("Press 'v' to view the commit stats or 'a' to add one: ")
+
+if ask == 'v':
+    view()
+    close()
+
+if ask == 'a':
+    add()
+    close()
+
+
+
 
