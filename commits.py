a06b2179f6eccb23440ce9188ba13d3a278c6082
jaymalave <jaym.github@gmail.com>
2021-07-08 21:29:22 +0530

    added search feature

--- commits.py
+++ commits.py
@@ -10,7 +10,7 @@ def add():
  cursor.execute('INSERT INTO commits(DATE, COMMITS) VALUES (?, ?)', (  date  ,  commits   ))
 
  conn.commit()   
- print("success")
+ print("Commits Added!")
 
 def view():
   for row in cursor.execute('SELECT * FROM commits'):
@@ -18,11 +18,20 @@ def view():
       commits = row[1]
       print(date, ':', commits)
 
+def search():
+   query = input("Enter the date you want to know the commits of: ")
+
+   for row in cursor.execute('SELECT * FROM commits WHERE DATE = ?', (query,)):
+      date = row[0]
+      commits = row[1]
+      print(date, ':', commits)
+
+
 
 def close():
   conn.close()
 
-ask = input("Press 'v' to view the commit stats or 'a' to add one: ")
+ask = input("Press 'v' to view the commit stats, 's' to search for a particular date or 'a' to add one: ")
 
 if ask == 'v':
     view()
@@ -31,6 +40,10 @@ if ask == 'v':
 if ask == 'a':
     add()
     close()
+
+if ask == 's':
+  search()
+  close()
 
 
 
